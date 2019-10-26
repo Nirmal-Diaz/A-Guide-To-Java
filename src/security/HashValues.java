7434750d52ed2b0fad9b8847934b81ca458357ce
Nirmal Diaz <nirmaldiaz@gmail.com>
2019-10-26 06:34:45 -0700

    Updated: HashValues.java

--- src/security/HashValues.java
+++ src/security/HashValues.java
@@ -12,15 +12,14 @@ class HashValues {
     //NOTE: Java "hash values" are known as "message digests"
     static byte[] generateHash(byte[] message, String algorithm) {
         MessageDigest messageDigest;
-        byte[] digest = null;
         try {
             messageDigest = MessageDigest.getInstance(algorithm);
             messageDigest.update(message);
-            digest = messageDigest.digest();
+            return messageDigest.digest();
         } catch (NoSuchAlgorithmException nsae) {
             System.out.println("NoSuchAlgorithmException occurred");
         }
-        return digest;
+        return null;
     }
 
     static void convertHashToHex(byte[] digest) {
@@ -32,18 +31,22 @@ class HashValues {
     }
 
     //NOTE: "DigestInputStream" is best suited for generating hash values for files
-    static byte[] demonstrateDigestInputStream(File file, MessageDigest messageDigest) {
+    //NOTE: Use on() on DigestInputStream to partially read files and generate hashes
+    static MessageDigest demonstrateDigestInputStream(File file, String algorithm) {
         //NOTE: A "DigestInputStream" has a ByteInputStream and a MessageDigest
-        try (DigestInputStream digestInputStream = new DigestInputStream(new FileInputStream(file), messageDigest) ) {
+        try (DigestInputStream digestInputStream = new DigestInputStream(new FileInputStream(file), MessageDigest.getInstance(algorithm))) {
             while (digestInputStream.read() != -1) {
                 //NOTE: When we call read() on the "DigestInputStream", it automatically reads the ByteInputStream and update() the MessageDigest
                 //NOTE: This is an empty loop. All the work is done inside the expression area
             }
             return digestInputStream.getMessageDigest();
+        } catch (NoSuchAlgorithmException nsae) {
+            System.out.println("NoSuchAlgorithmException occurred");
         } catch (FileNotFoundException fnfe) {
             System.out.println("File not found: " + file.getAbsolutePath());
         } catch (IOException ioe) {
             System.out.println("IOException occurred");
         }
+        return null;
     }
 }
